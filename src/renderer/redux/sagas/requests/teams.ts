d1d280c4bb6429b1265d306ab865a047c4fc0588
magalhaesjr <jeffrey.magalhaes@gmail.com>
2023-05-13 16:36:41 -0400

    Fix bug with importing csv team codes

--- src/renderer/redux/sagas/requests/teams.ts
+++ src/renderer/redux/sagas/requests/teams.ts
@@ -8,12 +8,25 @@ const fetchTeams = async () => {
     return null;
   }
 
+  // Find the mapping code cell, since they change the name every so many years
+  let col = '';
+
+  ['Mapping Code', 'Team/Division IDs'].forEach((cName) => {
+    if (cName in teamSheets[0]) {
+      col = cName;
+    }
+  });
+
+  if (col.length === 0) {
+    throw new Error('Could not find mapping codes in csv');
+  }
+
   return teamSheets
     .filter((s: any) => s['Page Type'] === 'Team')
     .map((s: any, ind: number) => ({
       teamName: s['Page Title'],
       teamNumber: ind + 1,
-      mappingCode: s['Mapping Code'],
+      mappingCode: s[col],
     }));
 };
 
